66d8946932069b2ab3f9a35bcb2096385c824424
alps <alps@itu.dk>
2025-11-19 23:02:55 +0100

    Baseline CNN Test

---
+++ scripts/train_baseline_cnn.py
@@ -0,0 +1,12 @@
+# scripts/train_baseline_cnn.py
+import yaml
+from src.models.baseline_cnn import BaselineCNN
+from src.training.pipeline_manager import run_training_pipeline
+
+if __name__ == "__main__":
+    model_config_path = "configs/baseline_cnn.yaml"
+    run_training_pipeline(
+        model_class=BaselineCNN,
+        model_config_path=model_config_path,
+        project_name="NLP-mini-project"
+    )
