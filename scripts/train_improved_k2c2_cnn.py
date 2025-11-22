c059bb8a5bde01756126e6cc5c76c5b9c0ecdb78
alps <alps@itu.dk>
2025-11-22 16:08:47 +0100

    Small Update

--- scripts/train_improved_k2c2_cnn.py
+++ scripts/train_improved_k2c2_cnn.py
@@ -1,4 +1,4 @@
-# scripts/train_k2c2_cnn.py
+# scripts/train_improved_k2c2_cnn.py
 
 from src.training.pipeline_manager import run_training_pipeline
 from src.models.improved_k2c2_cnn import ImprovedK2C2CNN
@@ -6,6 +6,6 @@ from src.models.improved_k2c2_cnn import ImprovedK2C2CNN
 if __name__ == "__main__":
     run_training_pipeline(
         model_class=ImprovedK2C2CNN,
-        model_config_path="configs/k2c2_cnn.yaml",
+        model_config_path="configs/improved_k2c2_cnn.yaml",
         project_name="NLP-mini-project"
     )
