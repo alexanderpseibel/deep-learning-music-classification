7b00a9666b5b8ffb2f8398ea5520c5bca93e92a5
alps <alps@itu.dk>
2025-12-02 11:37:54 +0100

    Update Read Me and Delete old folders

--- scripts/old_train_files/train_paper_k2c2_cnn.py
+++
@@ -1,9 +0,0 @@
-from src.training.pipeline_manager import run_training_pipeline
-from src.models.old_models.paper_k2c2_cnn import PaperK2C2CNN
-
-if __name__ == "__main__":
-    run_training_pipeline(
-        model_class=PaperK2C2CNN,
-        model_config_path="configs/old_configs/paper_k2c2_cnn.yaml",
-        project_name="NLP-mini-project"
-    )
